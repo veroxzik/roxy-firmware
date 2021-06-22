8e42c2a2dec5a6e2e8fd87fbfe176cd250172e18
veroxzik <43590004+veroxzik@users.noreply.github.com>
2021-06-21 23:09:14 -0400

    Working RGB buttons for Roxy v2.0

--- roxy/rgb/rgb_buttons.h
+++ roxy/rgb/rgb_buttons.h
@@ -23,6 +23,10 @@
 
 class Rgb_Buttons {
     private:
+        enum Timer {
+            Timer1,
+            Timer8
+        };
         uint8_t global_brightness = 255;
         CHSV led_color[MAX_BUTTONS];
         bool enabled[MAX_BUTTONS];
@@ -31,6 +35,7 @@ class Rgb_Buttons {
         bool init_pin_set;
         volatile uint32_t* dma_src;
         volatile uint8_t current_led = 0;
+        volatile Timer current_timer;
         volatile uint32_t cnt;
         volatile bool need_update[MAX_BUTTONS];
         volatile bool busy;
@@ -39,16 +44,29 @@ class Rgb_Buttons {
             cnt--;
 
             // TODO: Only TIM1 supported
-            DMA1.reg.C[4].NDTR = 26;
-			DMA1.reg.C[4].MAR = (uint32_t)&dmabuf;
-			DMA1.reg.C[4].PAR = (uint32_t)dma_src;
-			DMA1.reg.C[4].CR = 	(0 << 10) |	// MSIZE = 8-bits 
-								(1 << 8) | 	// PSIZE = 16-bits
-								(1 << 7) | 	// Memory increment mode enabled
-								(0 << 6) | 	// Peipheral increment mode disabled
-								(1 << 4) | 	// Direction: read from memory
-								(1 << 1) | 	// Transfer complete interrupt enable
-								(1 << 0);	// Channel enable
+            if(current_timer == Timer1) {
+                DMA1.reg.C[4].NDTR = 26;
+                DMA1.reg.C[4].MAR = (uint32_t)&dmabuf;
+                DMA1.reg.C[4].PAR = (uint32_t)dma_src;
+                DMA1.reg.C[4].CR = 	(0 << 10) |	// MSIZE = 8-bits
+                                    (1 << 8) | 	// PSIZE = 16-bits
+                                    (1 << 7) | 	// Memory increment mode enabled
+                                    (0 << 6) | 	// Peipheral increment mode disabled
+                                    (1 << 4) | 	// Direction: read from memory
+                                    (1 << 1) | 	// Transfer complete interrupt enable
+                                    (1 << 0);	// Channel enable
+            } else {
+                DMA2.reg.C[0].NDTR = 26;
+                DMA2.reg.C[0].MAR = (uint32_t)&dmabuf;
+                DMA2.reg.C[0].PAR = (uint32_t)dma_src;
+                DMA2.reg.C[0].CR = 	(0 << 10) |	// MSIZE = 8-bits
+                                    (1 << 8) | 	// PSIZE = 16-bits
+                                    (1 << 7) | 	// Memory increment mode enabled
+                                    (0 << 6) | 	// Peipheral increment mode disabled
+                                    (1 << 4) | 	// Direction: read from memory
+                                    (1 << 1) | 	// Transfer complete interrupt enable
+                                    (1 << 0);	// Channel enable
+            }
         }
 
         void set_color(uint8_t r, uint8_t g, uint8_t b) {
@@ -85,9 +103,99 @@ class Rgb_Buttons {
             }
         }
 
+        // Set pin to Timer AF if active, or high otherwise
+        void set_pin(uint8_t index, bool state) {
+            if(index >= MAX_BUTTONS) {
+                return;
+            }
+
+            if(state) {
+                uint8_t af = 4;
+                current_timer = Timer8;
+                switch(index) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 5:
+                    case 6:
+                        af = 6;
+                        current_timer = Timer1;
+                        break;
+                    default:
+                        break;
+                }
+                current_pins->get_button_led(index)->set_af(af);
+                current_pins->get_button_led(index)->set_mode(Pin::AF);
+                current_pins->get_button_led(index)->set_pull(Pin::PullNone);
+            } else {
+                // When not actively writing to this pin, pull it high
+                current_pins->get_button_led(index)->set_mode(Pin::Output);
+                current_pins->get_button_led(index)->on();
+            }
+        }
+
+        // Enable relevant timer pins
+        void set_output(uint8_t index) {
+            if(index >= MAX_BUTTONS) {
+                return;
+            }
+            TIM1.CCER = 0;
+            TIM8.CCER = 0;
+            switch(index) {
+                case 0:
+                    TIM1.CCER = (1 << 1) | (1 << 0);    // CC1 output enable, invert polarity
+                    dma_src = &TIM1.CCR1;
+                    break;
+                case 1:
+                    TIM1.CCER = (1 << 5) | (1 << 4);    // CC2 output enable, invert polarity
+                    dma_src = &TIM1.CCR2;
+                    break;
+                case 2:
+                    TIM1.CCER = (1 << 9) | (1 << 8);    // CC3 output enable, invert polarity
+                    dma_src = &TIM1.CCR3;
+                    break;
+                case 3:
+                    TIM8.CCER = (1 << 7) | (1 << 6);    // CC2N output enable, invert polarity
+                    dma_src = &TIM8.CCR2;
+                    break;
+                case 4:
+                    TIM1.CCER = (1 << 3) | (1 << 2);    // CC1N output enable, invert polarity
+                    dma_src = &TIM1.CCR1;
+                    break;
+                case 5:
+                    TIM1.CCER = (1 << 7) | (1 << 6);    // CC2N output enable, invert polarity
+                    dma_src = &TIM1.CCR2;
+                    break;
+                case 6:
+                    TIM1.CCER = (1 << 11) | (1 << 10);  // CC3N output enable, invert polarity
+                    dma_src = &TIM1.CCR3;
+                    break;
+                case 7:
+                    TIM8.CCER = (1 << 1) | (1 << 0);    // CC1 output enable, invert polarity
+                    dma_src = &TIM8.CCR1;
+                    break;
+                case 8:
+                    TIM8.CCER = (1 << 5) | (1 << 4);    // CC2 output enable, invert polarity
+                    dma_src = &TIM8.CCR2;
+                    break;
+                case 9:
+                    TIM8.CCER = (1 << 9) | (1 << 8);    // CC3 output enable, invert polarity
+                    dma_src = &TIM8.CCR3;
+                    break;
+                case 10:
+                    TIM8.CCER = (1 << 13) | (1 << 12);    // CC4 output enable, invert polarity
+                    dma_src = &TIM8.CCR4;
+                    break;
+                case 11:
+                    break;
+            }
+
+        }
+
     public:
         void init() {
             RCC.enable(RCC.TIM1);
+            RCC.enable(RCC.TIM8);
             RCC.enable(RCC.DMA1);
 
             Interrupt::enable(Interrupt::DMA1_Channel5);
@@ -96,7 +204,7 @@ class Rgb_Buttons {
 			TIM1.CCR1 = 0;
             TIM1.CCR2 = 0;
             TIM1.CCR3 = 0;
-			
+
             TIM1.CCMR1 = (6 << 4 ) | (1 << 3);  // Channel 1 PWM mode, Preload Enabled
             TIM1.CCMR1 |= (6 << 12) | (1 << 11);// Channel 2 PWM mode, Preload Enabled
 			TIM1.CCMR2 = (6 << 4) | (1 << 3);   // Channel 3 PWM Mode, Preload Enabled
@@ -104,6 +212,23 @@ class Rgb_Buttons {
 			TIM1.BDTR = 1 << 15;	// Main output enable
 
             TIM1.CR1 = 1 << 0;      // Enable counter
+
+            Interrupt::enable(Interrupt::DMA2_Channel1);
+
+            TIM8.ARR = (72000000 / 800000) - 1; // period = 90, 0 = 29, 1 = 58
+			TIM8.CCR1 = 0;
+            TIM8.CCR2 = 0;
+            TIM8.CCR3 = 0;
+            TIM8.CCR4 = 0;
+
+            TIM8.CCMR1 = (6 << 4 ) | (1 << 3);  // Channel 1 PWM mode, Preload Enabled
+            TIM8.CCMR1 |= (6 << 12) | (1 << 11);// Channel 2 PWM mode, Preload Enabled
+			TIM8.CCMR2 = (6 << 4) | (1 << 3);   // Channel 3 PWM Mode, Preload Enabled
+            TIM8.CCMR2 |= (6 << 12) | (1 << 11);// Channel 4 PWM Mode, Preload Enabled
+			TIM8.DIER = 1 << 8;		// Update DMA request enable
+			TIM8.BDTR = 1 << 15;	// Main output enable
+
+            TIM8.CR1 = 1 << 0;      // Enable counter
         }
 
         void enable(uint8_t index) {
@@ -119,34 +244,6 @@ class Rgb_Buttons {
                 set_output(index);
                 current_led = index;
                 init_pin_set = true;
-            }
-        }
-
-        void set_pin(uint8_t index, bool state) {
-            if(index >= MAX_BUTTONS) {
-                return;
-            }
-
-            if(state) {
-                uint8_t af = 4;
-                switch(index) {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 5:
-                    case 6:
-                        af = 6;
-                        break;
-                    default:
-                        break;
-                }
-                current_pins->get_button_led(index)->set_af(af);
-                current_pins->get_button_led(index)->set_mode(Pin::AF);
-                current_pins->get_button_led(index)->set_pull(Pin::PullNone);
-            } else {
-                // When not actively writing to this pin, pull it high
-                current_pins->get_button_led(index)->set_mode(Pin::Output);
-                current_pins->get_button_led(index)->on();
             }
         }
 
@@ -214,53 +311,6 @@ class Rgb_Buttons {
             need_update[index] = true;
         }
 
-        void set_output(uint8_t index) {
-            if(index >= MAX_BUTTONS) {
-                return;
-            }
-            TIM1.CCER = 0;
-            switch(index) {
-                case 0:
-                    TIM1.CCER = (1 << 1) | (1 << 0);    // CC1 output enable, invert polarity
-                    dma_src = &TIM1.CCR1;
-                    break;
-                case 1:
-                    TIM1.CCER = (1 << 5) | (1 << 4);    // CC2 output enable, invert polarity
-                    dma_src = &TIM1.CCR2;
-                    break;
-                case 2:
-                    TIM1.CCER = (1 << 9) | (1 << 8);    // CC3 output enable, invert polarity
-                    dma_src = &TIM1.CCR3;
-                    break;
-                case 3:
-                    
-                    break;
-                case 4:
-                    TIM1.CCER = (1 << 3) | (1 << 2);    // CC1N output enable, invert polarity
-                    dma_src = &TIM1.CCR1;
-                    break;
-                case 5:
-                    TIM1.CCER = (1 << 7) | (1 << 6);    // CC2N output enable, invert polarity
-                    dma_src = &TIM1.CCR2;
-                    break;
-                case 6:
-                    TIM1.CCER = (1 << 11) | (1 << 10);  // CC3N output enable, invert polarity
-                    dma_src = &TIM1.CCR3;
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    break;
-            }
-
-        }
-
         // This interrupt routine fires from buttons_leds_manager (TIM6) every 0.05ms.
         // It schedules single LED bursts if an update is required.
         void irq() {
@@ -279,9 +329,14 @@ class Rgb_Buttons {
             }
         }
 
-        void irq_dma() {
-            DMA1.reg.C[4].CR = 0;
-            DMA1.reg.IFCR = 1 << 16;
+        void irq_dma(Interrupt::IRQ _interrupt) {
+            if(_interrupt == Interrupt::DMA1_Channel5) {
+                DMA1.reg.C[4].CR = 0;
+                DMA1.reg.IFCR = 1 << 16;
+            } else if(_interrupt == Interrupt::DMA2_Channel1) {
+                DMA2.reg.C[0].CR = 0;
+                DMA2.reg.IFCR = 1 << 0;
+            }
 
             if(cnt) {
                 schedule_dma();
@@ -296,7 +351,12 @@ Rgb_Buttons rgb_buttons;
 
 template<>
 void interrupt<Interrupt::DMA1_Channel5>() {
-	rgb_buttons.irq_dma();
+	rgb_buttons.irq_dma(Interrupt::DMA1_Channel5);
+}
+
+template<>
+void interrupt<Interrupt::DMA2_Channel1>() {
+    rgb_buttons.irq_dma(Interrupt::DMA2_Channel1);
 }
 
 #endif
